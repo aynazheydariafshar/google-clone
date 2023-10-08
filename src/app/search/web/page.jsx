33b95354ba935e66c974d46ec15a8d85e3ccfb84
aynaz <aynaz@arianalabs.io>
2023-10-08 11:48:52 +0330

    FEAT : handle error on web pages

--- src/app/search/web/page.jsx
+++ src/app/search/web/page.jsx
@@ -1,9 +1,25 @@
-// "use client";
+import Link from "next/link";
+
 export default async function WebPage({ searchParams }) {
   const response = await fetch(
     `https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${searchParams.searchTerm}`
   );
+
+  if (!response.ok) throw new Error("have problem");
   const data = await response.json();
   const results = data.items;
+  if (!results) {
+    return (
+      <div className="flex flex-col justify-center items-center pt-10">
+        <h1 className="text-3xl p-4">No Results Find</h1>
+        <p className="text-lg">
+          Try searching for something else or go back to the homepage
+        </p>
+        <Link className="text-blue-500" href="/">
+          HOME
+        </Link>
+      </div>
+    );
+  }
   return <>{results && results.map((item) => <h1>{item.title}</h1>)}</>;
 }
