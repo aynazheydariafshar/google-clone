cb4acad1ec461055f3515ad4b99f7e80ee45881c
aynaz <aynaz@arianalabs.io>
2023-10-08 11:34:01 +0330

    FEAT : fetch data from google search api

--- src/app/search/web/page.jsx
+++ src/app/search/web/page.jsx
@@ -1,3 +1,9 @@
-export default function WebPage() {
-  return <div>WebPage</div>;
+// "use client";
+export default async function WebPage({ searchParams }) {
+  const response = await fetch(
+    `https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${searchParams.searchTerm}`
+  );
+  const data = await response.json();
+  const results = data.items;
+  return <>{results && results.map((item) => <h1>{item.title}</h1>)}</>;
 }
